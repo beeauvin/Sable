68acafb4ffa56ec50698feb9738fd782a1b6afcd
Bee <79487947+beeauvin@users.noreply.github.com>
2025-04-21 11:27:49 -0700

    docs(license): update license headers (#32)
    use the full naming convention

--- Tests/PulseTests/Extensions/Pulse/FluentBuilder.swift
+++ Tests/PulseTests/Extensions/Pulse/FluentBuilder.swift
@@ -1,4 +1,4 @@
-// Copyright © 2025 Cassidy Spring. 🖤 Sable Project.
+// Copyright © 2025 Cassidy Spring (Bee). 🖤 Sable Project.
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
